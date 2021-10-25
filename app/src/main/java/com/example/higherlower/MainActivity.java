3f83508e692eb3d446f53b452d442e8fc13ed0ce
Iskandarkenz <miskandarrasyid11@gmail.com>
2021-10-25 19:36:45 +0700

    first commit

---
+++ app/src/main/java/com/example/higherlower/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.higherlower;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.Random;
+
+public class MainActivity<int_randomNumber> extends AppCompatActivity {
+    int randomNumber;
+    public void makeToast(String string) {
+        Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();
+    }
+    public void guess(View view) {
+        EditText guessEditTextNumber = (EditText) findViewById(R.id.editTextNumber);
+
+        int guessInt = Integer.parseInt(guessEditTextNumber.getText().toString());
+
+        if (guessInt > randomNumber) {
+            makeToast("Lower!");
+        }else if (guessInt < randomNumber) {
+            makeToast("Higher!");
+        }else {
+            makeToast("That's right! Try again!");
+            Random rand = new Random();
+            randomNumber = rand.nextInt(25) + 1;
+        }
+
+        // Toast.makeText(MainActivity.this, guessEditTextNumber.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Random rand = new Random();
+        randomNumber = rand.nextInt(25) + 1;
+    }
+
+}
